1dfe5e0c85589f11718713986defe0c148fa7c65
James Chen <dumganhar@gmail.com>
2025-01-14 16:43:16 +0800

    [v3.8.6] Inline enums for CCObjectFlags.

--- cocos/serialization/instantiate.ts
+++ cocos/serialization/instantiate.ts
@@ -24,13 +24,13 @@
 */
 
 import { DEV, JSB } from 'internal:constants';
-import { CCObject, isCCObject, js, ValueType, jsbUtils, isCCClassOrFastDefined, getError, warn, misc, cclegacy } from '../core';
+import { CCObject, CCObjectFlags, isCCObject, js, ValueType, jsbUtils, isCCClassOrFastDefined, getError, warn, misc, cclegacy } from '../core';
 import { Prefab } from '../scene-graph/prefab';
 import { Node } from '../scene-graph/node';
 import { Component } from '../scene-graph/component';
 
-const Destroyed = CCObject.Flags.Destroyed;
-const PersistentMask = CCObject.Flags.PersistentMask;
+const Destroyed = CCObjectFlags.Destroyed;
+const PersistentMask = CCObjectFlags.PersistentMask;
 
 const objsToClearTmpVar: any[] = [];   // used to reset _iN$t variable
 
