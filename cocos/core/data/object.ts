ea9bda083f52bff49541ad6485bb39c821b9aadb
James Chen <dumganhar@gmail.com>
2025-01-16 12:52:14 +0800

    Add comments for why adds `@dontmangle`

--- cocos/core/data/object.ts
+++ cocos/core/data/object.ts
@@ -197,7 +197,11 @@ class CCObject implements EditorExtendableObject {
      */
     public _objFlags: number = 0;
 
-    /** @dontmangle */
+    /**
+     * @dontmangle
+     * NOTE: _name is a serializable property set by `CCClass.fastDefine`,
+     * so it should not be mangled while `mangleProtected` is true in `<<ProjectRoot>>/engine-mangle-config.json`.
+     */
     protected declare _name: string;
 
     constructor (name = '') {
