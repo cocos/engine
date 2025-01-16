c199ca71027774248bd556e93a728526d257bc3a
Zhou Zhenglong <zlzhou.sh@outlook.com>
2025-01-16 18:00:57 +0800

    improve resize

--- cocos/rendering/custom/layout-graph-utils.ts
+++ cocos/rendering/custom/layout-graph-utils.ts
@@ -546,7 +546,7 @@ export function initializeDescriptorSetLayoutInfo (
     layoutData: DescriptorSetLayoutData,
     info: DescriptorSetLayoutInfo,
 ): void {
-    info.bindings = [];
+    info.bindings.length = 0;
     for (let i = 0; i < layoutData.descriptorBlocks.length; ++i) {
         const block = layoutData.descriptorBlocks[i];
         let slot = block.offset;
